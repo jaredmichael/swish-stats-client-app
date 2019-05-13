7466af522be31fdbda98517f2979f463207f03d8
Jared Michael A Juan <jaredmichael.a_juan@yahoo.com>
2019-05-13 16:14:26 -0700

    updated config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL =
     process.env.REACT_APP_API_BASE_URL || 
-    'mongodb://my-new-user:mnu_pw-001@ds127851.mlab.com:27851/swish-stats';
+    'https://swish-stats-server-app.herokuapp.com/api'
